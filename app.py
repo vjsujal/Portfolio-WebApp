aa98a0cc00138640da1bb63742276afd5717e012
vjsujal <www.sujalvj121@gmail.com>
2024-02-23 20:59:06 +0530

    Switched to Gemini

--- app.py
+++ app.py
@@ -1,55 +1,77 @@
-import os
 from flask import Flask, render_template, request, jsonify
-from langchain.chat_models import ChatOpenAI
-from chat import generate_response
-from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader, PromptHelper, LLMPredictor
-import openai
-import threading
+import google.generativeai as genai
+import json
+import os
+
+genai.configure(api_key=os.environ['GENIAI_API_KEY'])
+
+# Set up the model
+generation_config = {
+  "temperature": 0.3,
+  "top_p": 1,
+  "top_k": 1,
+  "max_output_tokens": 1024,
+}
+
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+]
+
+model = genai.GenerativeModel(model_name="gemini-1.0-pro",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
+
+convo = model.start_chat(history=[
+  {
+    "role": "user",
+    "parts": ["Introduction: Begin by welcoming users to Sujal Vijaivargia's personal portfolio website and introduce yourself as the Chatbot assistent specifically designed to assist with queries related to Sujal.Purpose: Clarify that your primary role is to provide detailed information about Sujal's background, projects, skills, achievements, and other relevant details.Scope: Emphasize that you are here to assist with inquiries solely related to Sujal. Any questions or requests unrelated to Sujal should be directed elsewhere.Navigation Guidance: Guide users on how to navigate the website to find specific information about Sujal, such as educational background, projects, experience, skills, achievements, and certifications.Prompting for Questions: Encourage users to ask any questions they may have about Sujal. Assure them that you're here to provide comprehensive answers and assistance.Prompting for Additional Assistance: Inform users that if they require further assistance or have specific inquiries not covered by the provided information, they can always reach out for additional help. Answer the user in 1 to 2 lines only if not asked for deatails. Comprehensive Set of Information Related to Sujal: Social Media Handle: LinkedIn:https://www.linkedin.com/in/sujalvijayvargiya Github:https://github.com/vjsujal Education:Institution: SRM University Amaravati, Andhra PradeshDegree: Bachelor of Technology in Artificial Intelligence and Machine LearningGPA: 8.93/10.0 (till 5th Sem)Expected Graduation: June 2025Relevant Coursework: Algorithms and Data Structure, Database Management System, Operating System, Computer Network, Linear Algebra, Discrete Mathematics, Artificial IntelligenceSenior Secondary School: St. Pauls SchoolPercentage: 83.8%Projects:Full Stack Fashion Recommender with Chatbot integrationChurn PredictionPortfolio Website (Django Project)Experience:Research Intern at Deakin University, Australia (Animal Face Detection/Recognition)Technical Skills:Programming Languages: Python, Java, C/C++Web Development: HTML/CSS, Django, JavaScript, React JsData Science and Miscellaneous Technologies: Data Science Pipeline, LinuxMachine Learning and Deep Learning: Regression, Classification, CNN, Neural Networks, Decision tree, NLP (Basic)Database: MySQLAchievements:District level Gold Medalist Gymnast100% Scholarship at SRM University APCertificates:Machine Learning SpecializationSupervised Machine Learning: Regression and ClassificationUnsupervised Learning, Recommenders, Reinforcement LearningPython Crash CoursePython Fundamentals"]
+  },
+  {
+    "role": "model",
+    "parts": ["Greetings, I'm here to assist you with inquiries related to Sujal Vijaivargia, an individual excelling in Artificial Intelligence and Machine Learning. Ask me about his background, projects, skills, and achievements."]
+  },
+])
 
 
-documents = SimpleDirectoryReader('./data').load_data()
 
 app = Flask(__name__)
 
-# Initialize prompt_helper and index
-llm_predictor = None
-prompt_helper = None
-index = None
 
 @app.route("/")
 def index_get():
-    threading.Thread(target=initialize).start()
     script_root = request.script_root
     return render_template("index.html", script_root=script_root)
 
-# @app.route("/initialize")
-def initialize():
-    print("Index initialized")
-    global llm_predictor, prompt_helper, index
-    
-    # Initialize llm_predictor
-    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.1, model_name="ada-search-document"))
 
-    # Initialize prompt_helper and index
-    max_input_size = 4096
-    num_output = 8
-    max_chunk_overlap = 0.2
-    prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
-    index = GPTVectorStoreIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
-    
-    # Store index in global scope
-    globals()["index"] = index
 
 @app.post("/predict")
 def predict():
-    if index is None:
-        return "Index not initialized. Please call the /initialize endpoint first."
-    
-    text = request.get_json().get("message")
-    # Generate response
-    response = generate_response(index, text.lower())
-    message = {"answer" : response}
-    return jsonify(message)
+    data = request.json
+    print(data)
+    message = data['message']
+    global convo
+    try:
+      convo.send_message(message)
+      print(convo.last.text)
+      reply = convo.last.text
+    except Exception as e:
+      reply = "Something went wrong. Please try again."
+    return jsonify({"answer": reply})
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
